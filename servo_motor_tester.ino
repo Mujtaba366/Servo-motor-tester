1158e07ab01baf29efd5730a6747483b3e603fbb
Mujtaba366 <150569273+Mujtaba366@users.noreply.github.com>
2026-05-27 18:47:06 +1200

    Add servo motor control code

---
+++ servo_motor_tester.ino
@@ -0,0 +1,17 @@
+#include <Servo.h>
+Servo myServo;
+void setup() {
+  myServo.attach(9);
+}
+void loop() {
+  // 0 → 180
+  for (int pos = 0; pos <= 180; pos++) {
+    myServo.write(pos);
+    delay(15);
+  }
+  // 180 → 0
+  for (int pos = 180; pos >= 0; pos--) {
+    myServo.write(pos);
+    delay(15);
+  }
+}
